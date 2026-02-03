f93c6f4eeb98e438fd02620d63716c3fe5a75883
Yadgarov Fayzullo <yadgarovfayzullo0@gmail.com>
2026-02-04 01:48:35 +0500

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono, DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const dmSans = DM_Sans({subsets:['latin'],variable:'--font-sans'});
+const dmSans = DM_Sans({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,7 +17,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Scalefield - Scale Your SaaS Without Limits",
-  description: "Build, deploy, and scale your SaaS applications with ease. Focus on what matters while we handle the infrastructure.",
+  description:
+    "Build, deploy, and scale your SaaS applications with ease. Focus on what matters while we handle the infrastructure.",
 };
 
 export default function RootLayout({
@@ -27,6 +28,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={dmSans.variable} suppressHydrationWarning>
+           <head>
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
